b8a0f933da377f01ae4fa369e841c7a51586d774
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-12-29 13:27:30 +0530

    added time selection tool

--- Components/TimeSelector.jsx
+++ Components/TimeSelector.jsx
@@ -1,70 +1,86 @@
+import { useState } from "react";
 import { View, Text, StyleSheet, TouchableHighlight, ScrollView } from "react-native";
 export default function () {
+    const [selectedHours, setSelectedHours] = useState(new Array(24).fill(0).map((i, j) => [j + 1, 0]));
     return (
         <View style={styles.wrapper}>
             <Text style={styles.restime}>Mark your restricted time period</Text>
-            <ScrollView style={styles.scview} contentContainerStyle={{ alignItems: 'center' }}>
-                {
-                    new Array(24).fill(1).map((i, j) => (
-                        <View key={j} style={styles.hourmap}>
-                            <View style={styles.hourline} />
-                            <TouchableHighlight style={styles.hourbubble}>
-                                <Text style={styles.hourtext}>{j + 1}</Text>
-                            </TouchableHighlight>
-                            <View style={styles.hourline} />
-                        </View>
-                    ))
-                }
+            <ScrollView style={styles.scview} contentContainerStyle={{ alignItems: "center" }}>
+                {selectedHours.map(([i, j]) => (
+                    <View key={i} style={styles.hourmap}>
+                        <View style={[styles.hourline, j ? styles.selected : styles.unselected]} />
+                        <TouchableHighlight
+                            underlayColor={"red"}
+                            style={[styles.hourbubble, j ? styles.selected : styles.unselected]}
+                            onPress={() => {
+                                setSelectedHours(
+                                    selectedHours.map(([hour, state]) => {
+                                        if (hour == i) return [hour, !state];
+                                        return [hour, state];
+                                    })
+                                );
+                            }}
+                        >
+                            <Text style={styles.hourtext}>{i}</Text>
+                        </TouchableHighlight>
+                        <View style={[styles.hourline, j ? styles.selected : styles.unselected]} />
+                    </View>
+                ))}
             </ScrollView>
             <Text style={styles.donebt}>Done</Text>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    wrapper:{
-        backgroundColor : "#252525",
-        borderWidth : 1,
-        borderColor : 'purple',
-        alignItems : 'center',
-        paddingVertical : 10,
-        paddingHorizontal : 30,
-        borderRadius : 10,
+    wrapper: {
+        backgroundColor: "#252525",
+        borderWidth: 1,
+        borderColor: "purple",
+        alignItems: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 10,
     },
-    restime : {
-        color : 'white',
-        fontSize : 20
-    },  
+    restime: {
+        color: "white",
+        fontSize: 20,
+    },
     scview: {
         margin: 10,
-        borderColor: 'purple',
-        borderRadius: 10
+        borderColor: "purple",
+        borderRadius: 10,
     },
     hourwrap: {
         height: `${100 / 24}%`,
     },
     hourline: {
-        backgroundColor: 'purple',
+        backgroundColor: "purple",
         width: 2,
         height: 7,
-        alignSelf: 'center'
+        alignSelf: "center",
     },
     hourbubble: {
-        backgroundColor: 'purple',
         width: 30,
         borderRadius: 15,
         aspectRatio: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    selected: {
+        backgroundColor: "red",
+    },
+    unselected: {
+        backgroundColor: "purple",
     },
     hourtext: {
-        color: 'beige',
+        color: "beige",
     },
-    donebt:{
-        color : 'white',
-        backgroundColor : 'purple',
-        padding : 7,
-        borderRadius : 10,
-        fontSize : 20
-    }
-})
+    donebt: {
+        color: "white",
+        backgroundColor: "purple",
+        padding: 7,
+        borderRadius: 10,
+        fontSize: 20,
+    },
+});
