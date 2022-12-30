126a7faa842c581f632f15949b9262bf45abb04a
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-12-30 21:20:10 +0530

    added schedule generator

---
+++ utils/Generator.js
@@ -0,0 +1,42 @@
+const restricted = [6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17];
+const activities = ["React", "Angular", "Typescript", 'vue'];
+
+const genarate = (restricted, activities) => {
+
+    let dailyhours = 24 - restricted.length;
+    let dailyshare = Math.floor(dailyhours / activities.length);
+    let dailyweight = new Array(activities.length).fill(dailyshare);
+
+    let dremainder = dailyhours % activities.length;
+    for (let i = 0; i < dremainder; i++) dailyweight[i] += 1;
+
+    dailyweight = dailyweight.filter((i) => i != 0);
+
+    let schedule = new Array();
+    let dpointer = 0;
+    let apointer = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let hourleft = dailyhours;
+        let day = new Array();
+        while (hourleft > 0) {
+            day.push([(dpointer+apointer)%activities.length, dailyweight[dpointer%dailyweight.length]]);
+            hourleft -= dailyweight[dpointer%dailyweight.length];
+            dpointer++;
+        }
+        schedule.push(day);
+        apointer++;
+    }
+    // console.log(schedule);
+    return schedule.map(i=>{
+        let index = 0;
+        const day = new Array(24).fill(null);
+        return day.map((activity,j)=>{
+            if (restricted.includes(j)) return null;
+            if (i[index][1] == 0) index++;
+            i[index][1]--;
+            return i[index][0]
+        })
+    })
+};
+console.log(genarate(restricted, activities));
